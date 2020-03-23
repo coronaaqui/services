bda3c324766c54cef9f9775383a530f551ba7f95
Tassio Noronha <tassionoronha@gmail.com>
2020-03-23 14:33:44 -0300

    fix filter by region

--- cbrasil/cbrasil/helpers.py
+++ cbrasil/cbrasil/helpers.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from django.db.models import Q
 
+def get_region_filter(region__initial):
+    return (Q(events__region__initial=region__initial) | Q(events__city__region__initial=region__initial)) 
+
 def get_closed_events_filters(region__initial):
     today = datetime.today()
     if region__initial:
-        HAS_BOTH_CORRECT_DATES = Q((Q(events__region__initial=region__initial) | Q(events__city__region__initial=region__initial)),events__from_date__lte=today,events__from_date__gte=today)
-        HAS_UNDEFINED_ENDS_DATE = Q((Q(events__region__initial=region__initial) | Q(events__city__region__initial=region__initial)),events__from_date__lte=today,events__undefined_ends_date=True)
+        HAS_BOTH_CORRECT_DATES = Q(get_region_filter(region__initial),events__from_date__lte=today,events__from_date__gte=today)
+        HAS_UNDEFINED_ENDS_DATE = Q(get_region_filter(region__initial),events__from_date__lte=today,events__undefined_ends_date=True)
     else:
         HAS_BOTH_CORRECT_DATES = Q(events__from_date__lte=today,events__from_date__gte=today)
         HAS_UNDEFINED_ENDS_DATE = Q(events__from_date__lte=today,events__undefined_ends_date=True)
--- cbrasil/cbrasil/organizations/filters.py
+++ cbrasil/cbrasil/organizations/filters.py
@@ -4,22 +4,22 @@ from django.db.models.functions import Coalesce
 from rest_framework import filters
 
 from cbrasil.organizations.models import Sectors
-from cbrasil.helpers import get_closed_events_filters
+from cbrasil.helpers import get_closed_events_filters, get_region_filter
 
 class SectorsAggregationFilters(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         region__initial = request.GET.get('region__initial', None)
         actually_closed = request.GET.get('actually_closed', False)
+        has_both_correct_dates, has_undefined_ends_date = get_closed_events_filters(region__initial)
         q = Sectors.objects
+        
         if region__initial and actually_closed == 'True':
-            has_both_correct_dates, has_undefined_ends_date = get_closed_events_filters(region__initial)
             q = q.annotate(total_estimated_impact=Coalesce(Sum('events__estimated_impact', filter=has_both_correct_dates| has_undefined_ends_date), Value(0)))
         elif region__initial:
-            q = q.annotate(total_estimated_impact=Sum('events__estimated_impact', filter=Q(events__region__initial=region__initial)))
+            q = q.annotate(total_estimated_impact=Coalesce(Sum('events__estimated_impact', filter=Q(get_region_filter(region__initial))), Value(0)))
         elif actually_closed:
-            has_both_correct_dates, has_undefined_ends_date = get_closed_events_filters(region__initial)
-            q = q.annotate(total_estimated_impact=Sum('events__estimated_impact', filter=has_both_correct_dates| has_undefined_ends_date))
+            q = q.annotate(total_estimated_impact=Coalesce(Sum('events__estimated_impact', filter=has_both_correct_dates| has_undefined_ends_date), 0))
         else:
-            q = q.annotate(total_estimated_impact=Sum('events__estimated_impact'))
+            q = q.annotate(total_estimated_impact=Coalesce(Sum('events__estimated_impact'), Value(0)))
         return q.order_by('-total_estimated_impact', 'name')
